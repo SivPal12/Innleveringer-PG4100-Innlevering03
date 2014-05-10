afa10a0fd32ff108ec9d3382650c8fa9d3b1d2a4
Pål Sivertsen <paal@nith.no>
2014-05-11 01:09:12 +0200

    Implemented prime controller and view

--- src/main/java/no/nith/sivpal12/pg4100/controller/Prime.java
+++ src/main/java/no/nith/sivpal12/pg4100/controller/Prime.java
@@ -1,20 +1,23 @@
 package no.nith.sivpal12.pg4100.controller;
 
+import org.apache.commons.math3.primes.Primes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Prime {
 
-    @RequestMapping(value="prime", method=RequestMethod.GET)
-    public String setupPage() {
-        return "test";
-    }
-    
-    @RequestMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("test");
-    }
+	@RequestMapping(value="/")
+	public ModelAndView index(@RequestParam(required=false) final Integer number) {
+		if (number != null && number > 0) {
+			if (Primes.isPrime(number)) {
+				return new ModelAndView("prime").addObject("result", "It's a prime!");
+			} else {
+				return new ModelAndView("prime").addObject("result", "It's NOT a prime!");
+			}
+		}
+		return new ModelAndView("prime");
+	}
 }
---
+++ src/main/resources/jsp/prime.jsp
@@ -0,0 +1,21 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prime checker</title>
+</head>
+<body>
+
+<h1>Prime checker</h1>
+
+<form>
+	<label>Skriv inn er tall for å sjekke om det er primtall</label>
+	<input name="number" pattern="^[1-9]\d*$" title="Enter positive number"/>
+	<input type="submit" value="Send"/>
+</form>
+
+<c:out value="${result}"></c:out>
+
+</body>
+</html>
